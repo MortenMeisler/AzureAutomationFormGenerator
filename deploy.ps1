0f2964d1e4d27b74a0dbffb87d07f640f672924b
Anders Spælling <spaelling@gmail.com>
2018-11-19 20:15:12 +0100

    code for vars.ps1 and SQL sku options

--- deploy.ps1
+++ deploy.ps1
@@ -1,4 +1,17 @@
 Import-Module AzureRM
+Set-Location $PSScriptRoot
+
+<# 
+# Run below code to create a PS script containing the vars that we need. This file is being ignored by git so that
+# we do not accidentally expose them to Github
+@"
+$SubscriptionId = 'SUBSCRIPTION_ID'
+$TenantId = 'TENANT_ID'
+"@ | Out-File ".\vars.ps1"
+#>
+
+# dot source the vars file
+. ".\vars.ps1"
 
 try {
     Select-AzureRmSubscription -SubscriptionId $SubscriptionId | Out-Null
@@ -12,8 +25,6 @@ Select-AzureRmSubscription -SubscriptionId $SubscriptionId | Out-Null
 $ResourceGroupName = 'rg81e35VYltTU3'
 $Location = 'northeurope'
 
-Set-Location $PSScriptRoot
-
 New-AzureRmResourceGroup -Name $ResourceGroupName -Location $Location -Force | Out-Null
 
 $TemplateParameterObject = @{
@@ -24,8 +35,6 @@ $TemplateParameterObject = @{
     sqlServerName = 'sqlB8jHNFN8wusv'.ToLower()
     sqlServerLocation = $Location
     sqlServerAdminPassword = 'TQ4NkJRjo4ICZfC*8sm7!pnm'
-    automationAccountName = 'dXRgrIsj4Dd7FgW7tPL75YO5'
-    automationAccountResourceGroup = 'dXRgrIsj4Dd7FgW7tPL75YO5'
 }
 
 Test-AzureRmResourceGroupDeployment -ResourceGroupName $ResourceGroupName -TemplateFile ".\azuredeploy.json" -TemplateParameterObject $TemplateParameterObject
